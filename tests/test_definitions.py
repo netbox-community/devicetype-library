f15ec47c6c74506db396d15bc55e4d7d2696b103
Jeremy Stretch <stretch@packetlife.net>
2019-12-19 10:19:41 -0500

    Introduced unit tests for definition files

---
+++ tests/test_definitions.py
@@ -0,0 +1,30 @@
+import glob
+import json
+import pytest
+import yaml
+from jsonschema import validate
+
+
+def _get_definition_files():
+    """
+    Return a list of all definition files.
+    """
+    return [f for f in glob.glob("vendors/**/*.yaml", recursive=True)]
+
+
+# Initialize schema
+with open("tests/schema.json") as schema_file:
+    schema = json.loads(schema_file.read())
+
+
+@pytest.mark.parametrize("file_path", _get_definition_files())
+def test_definition(file_path):
+    """
+    Validate DeviceType definitions using the provided JSON schema.
+    """
+    # Read file
+    with open(file_path) as definition_file:
+        definition = yaml.load(definition_file.read(), Loader=yaml.SafeLoader)
+
+    # Run validation
+    validate(definition, schema=schema)
