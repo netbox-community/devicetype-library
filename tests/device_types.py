7b478ad0a90f744cdbd4b9f4fae0274ae09cb1ee
Daniel W. Anner <daniel.anner@danstechsupport.com>
2023-07-17 15:29:41 +0000

    broke component validation out to its own function. Adding logic for validating device power input.

--- tests/device_types.py
+++ tests/device_types.py
@@ -8,6 +8,7 @@ class DeviceType:
     def __init__(self, definition, file_path):
         self.file_path = file_path
         self.isDevice = True
+        self.definition = definition
         self.manufacturer = definition.get('manufacturer')
         self._slug_manufacturer = self._slugify_manufacturer()
         self.slug = definition.get('slug')
@@ -60,6 +61,27 @@ class DeviceType:
         KNOWN_SLUGS.add(self.slug)
         return True
 
+    def validate_power(self):
+        # Check if power-ports exists
+        if self.definition.get('power-ports', False):
+            return True
+
+        # Check if module-bays exists
+        if self.definition.get('module-bays', False):
+            # There is not a standardized way to define PSUs that are module bays, so we will just assume they are valid
+            return True
+
+        # Lastly, check if interfaces exists and has a poe_mode defined
+        interfaces = self.definition.get('interfaces', False)
+        if interfaces:
+            for interface in interfaces:
+                poe_mode = interface.get('poe_mode', "")
+                if poe_mode != "" and poe_mode == "pd":
+                    return True
+
+        self.failureMessage = f'{self.file_path} has does not appear to have a valid power source. Ensure either "power-ports" or "interfaces" with "poe_mode" is defined.'
+        return False
+
 class ModuleType:
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls)
@@ -67,6 +89,7 @@ class ModuleType:
     def __init__(self, definition, file_path):
         self.file_path = file_path
         self.isDevice = False
+        self.definition = definition
         self.manufacturer = definition.get('manufacturer')
         self.model = definition.get('model')
         self._slug_model = self._slugify_model()
@@ -95,5 +118,27 @@ def verify_filename(device: (DeviceType or ModuleType)):
     if not (filename == device._slug_model or filename == device._slug_part_number or filename == device.part_number.casefold()):
         device.failureMessage = f'{device.file_path} file name is invalid. Must be either the model "{device._slug_model}" or part_number "{device.part_number} / {device._slug_part_number}"'
         return False
+
+    return True
+
+def validate_components(component_types, device_or_module):
+    for component_type in component_types:
+        known_names = set()
+        defined_components = device_or_module.definition.get(component_type, [])
+        if not isinstance(defined_components, list):
+            device_or_module.failureMessage = f'{device_or_module.file_path} has an invalid definition for {component_type}.'
+            return False
+        for idx, component in enumerate(defined_components):
+            if not isinstance(component, dict):
+                device_or_module.failureMessage = f'{device_or_module.file_path} has an invalid definition for {component_type} ({idx}).'
+                return False
+            name = component.get('name')
+            if not isinstance(name, str):
+                device_or_module.failureMessage = f'{device_or_module.file_path} has an invalid definition for {component_type} name ({idx}).'
+                return False
+            if name in known_names:
+                device_or_module.failureMessage = f'{device_or_module.file_path} has duplicated names within {component_type} ({name}).'
+                return False
+            known_names.add(name)
 
     return True
