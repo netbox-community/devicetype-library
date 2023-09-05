b25e45e1b790227350ef15e644799a352348ec41
Daniel W. Anner <daniel.anner@danstechsupport.com>
2023-09-05 15:39:21 -0400

    Adding code to check if the all switch is called (#1603)

--- tests/definitions_test.py
+++ tests/definitions_test.py
@@ -1,4 +1,4 @@
-from test_configuration import COMPONENT_TYPES, IMAGE_FILETYPES, SCHEMAS, KNOWN_SLUGS, ROOT_DIR, USE_LOCAL_KNOWN_SLUGS, NETBOX_DT_LIBRARY_URL, KNOWN_MODULES, USE_UPSTREAM_DIFF
+from test_configuration import COMPONENT_TYPES, IMAGE_FILETYPES, SCHEMAS, KNOWN_SLUGS, ROOT_DIR, USE_LOCAL_KNOWN_SLUGS, NETBOX_DT_LIBRARY_URL, KNOWN_MODULES, USE_UPSTREAM_DIFF, PRECOMMIT_ALL_SWITCHES
 import pickle_operations
 from yaml_loader import DecimalSafeLoader
 from device_types import DeviceType, ModuleType, verify_filename, validate_components
@@ -7,6 +7,7 @@ import glob
 import json
 import os
 import tempfile
+import psutil
 from urllib.request import urlopen
 
 import pytest
@@ -105,7 +106,11 @@ def test_environment():
     if definition_files:
         pytest.skip("No changes to definition files found.")
 
-if USE_UPSTREAM_DIFF:
+EVALUATE_ALL = False
+if any(x in PRECOMMIT_ALL_SWITCHES for x in psutil.Process(os.getppid()).cmdline()):
+    EVALUATE_ALL = True
+
+if USE_UPSTREAM_DIFF and not EVALUATE_ALL:
     definition_files = _get_diff_from_upstream()
 else:
     definition_files = _get_definition_files()
@@ -119,6 +124,7 @@ else:
     repo = Repo.clone_from(url=NETBOX_DT_LIBRARY_URL, to_path=temp_dir.name)
     KNOWN_SLUGS = pickle_operations.read_pickle_data(f'{temp_dir.name}/tests/known-slugs.pickle')
     KNOWN_MODULES = pickle_operations.read_pickle_data(f'{temp_dir.name}/tests/known-modules.pickle')
+
 
 @pytest.mark.parametrize(('file_path', 'schema'), definition_files)
 def test_definitions(file_path, schema):
